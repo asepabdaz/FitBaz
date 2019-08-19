5fb2a1c425c1adb4565aa5f31aecbab3901e3f3b
Asepabdaz <asepabdaz@gmail.com>
2019-08-19 14:08:20 +0700

    Initial Commit

---
+++ FitBaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitBaz
+//
+//  Created by Hai on 19/08/19.
+//  Copyright © 2019 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
